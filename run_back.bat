90adceb7d16b6249db0108d29f20792b75d6ca9c
Wasawat.Peng <wasawat.peng@gmail.com>
2023-04-11 01:44:11 +0700

    edit scripts

--- run_back.bat
+++ run_back.bat
@@ -2,4 +2,5 @@ pip install virtualenv
 virtualenv venv
 call .\venv\Scripts\activate
 pip install -r requirements.txt
+copy /b/v/y %cd%\config\qradar.py %cd%\venv\Lib\site-packages\sigma\backends
 flask --app api.py run
